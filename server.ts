e381d201ebb229cab4bccf3bd46f17ef8631a47e
Tien Sang Nguyen <ntbs1798@gmail.com>
2021-06-04 23:45:54 +0700

    fixed errors in server.ts

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ app.set('port', port);
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+let server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
