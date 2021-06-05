ba4441ab2fe87a9dc0dd8f00f785974dddf4a0ff
Tien Sang Nguyen <ntbs1798@gmail.com>
2021-06-05 09:19:54 +0700

    created partial content pages

--- routes/index.ts
+++ routes/index.ts
@@ -5,31 +5,31 @@ export default router;
 /* GET home page. */
 router.get('/', function(req, res, next) 
 {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 /* GET home page. */
 router.get('/home', function(req, res, next) 
 {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 /* GET about page. */
 router.get('/about', function(req, res, next) 
 {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'About Us', page: 'about' });
 });
 /* GET projects page. */
 router.get('/projects', function(req, res, next) 
 {
-  res.render('index', { title: 'Projects' });
+  res.render('index', { title: 'Our Projects', page: 'projects' });
 });
 /* GET services page. */
 router.get('/services', function(req, res, next) 
 {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Our Services', page: 'services' });
 });
 /* GET contact page. */
 router.get('/contact', function(req, res, next) 
 {
-  res.render('index', { title: 'Contact' });
+  res.render('index', { title: 'Contact Us', page: 'contact' });
 });
 //module.exports = router;
