80b646b83aad62c6c5a1cb3889224ccbe9b59186
Tien Sang Nguyen <ntbs1798@gmail.com>
2021-06-20 18:56:27 +0700

    added authguard for secure pages

--- Server/Routes/contact.ts
+++ Server/Routes/contact.ts
@@ -3,21 +3,24 @@ const router = express.Router();
 export default router;
 import {DisplayAddPage, DisplayContactListPage, DisplayEditPage, ProcessAddPage, ProcessDeletePage, ProcessEditPage} from '../Controllers/contact';
 
+//import Util functions
+import {AuthGuard} from '../Util/index';
+
 /*GET contact-list page- with /contact-list */
 router.get('/', DisplayContactListPage)
 
 /* GET - display /clothing-list/add page. */
-router.get('/add', DisplayAddPage);
+router.get('/add', AuthGuard, DisplayAddPage);
 
 /* display edit/:id page - with /contact-list/edit:id*/
-router.get('/edit/:id', DisplayEditPage);
+router.get('/edit/:id', AuthGuard, DisplayEditPage);
 
 /* POST - process /clothing-list/add page */
-router.post('/add', ProcessAddPage);
+router.post('/add', AuthGuard, ProcessAddPage);
 
 /* POST - process /clothing-list/edit/:id page */
-router.post('/edit/:id', ProcessEditPage);
+router.post('/edit/:id', AuthGuard, ProcessEditPage);
 
 /* GET - process /clothing-list/delete/:id */
-router.get('/delete/:id', ProcessDeletePage);
+router.get('/delete/:id', AuthGuard, ProcessDeletePage);
 
